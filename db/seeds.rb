48800e5341678a3366c6e67c2963cf287f0daf83
Toni  Diep <adiep123@gmail.com>
2020-06-15 21:21:30 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@ Student.create(name: "April", grade: "10th")
 Student.create(name: "Luke", grade: "9th")
 Student.create(name: "Devon", grade: "11th")
 Student.create(name: "Sarah", grade: "10th")
+
+
+namespace :db do
+ 
+  ...
+ 
+  desc 'seed the database with some dummy data'
+  task :seed do
+    require_relative './db/seeds.rb'
+  end
+end
+
+desc 'drop into the Pry console'
+task :console => :environment do
+  Pry.start
+end
+ 
+ 
